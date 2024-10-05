4b8175cd829208c8bbded2bb4a9b63a2c5c18b4c
jack-git-png <jack.minayerdji@elev.ga.ntig.se>
2024-10-05 13:17:25 +0200

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
+def Welcome_Page():
 # Använding av css för att byta backgrounden (för streamlit har inte)
-Background_Page = '''
+    Background_Page = '''
     <style>
     [data-testid="stAppViewContainer"] {
         background-image: url("https://asset.gecdesigns.com/img/wallpapers/beautiful-fantasy-wallpaper-ultra-hd-wallpaper-4k-sr10012418-1706506236698-cover.webp");
@@ -15,6 +16,7 @@ Background_Page = '''
     [data-testid="stToolbar"] {
         right: 2rem;
     }
+
     [data-testid="stVerticalBlockBorderWrapper"] {
         background: linear-gradient(135deg, rgba(255,255,255,0.18));
         backdrop-filter: blur(10px);
@@ -29,34 +31,33 @@ Background_Page = '''
         display: contents;
         padding: 10%
     }
+
     [data-testid="stTextInputRootElement"] {
         border-radius: 15px;
         border-color: white;
     }
-    [data-testid="stTextInputRootElement"]:hover{
-        border-color: grey;
-    }
-    [id="text_input_1"]{
-        background-color: black;
-    }
     [data-testid="stBaseButton-secondary"] {
         border-color: white;
         margin-top: 20px;
-        width: 120px;
-        background-color: black;
+        width: 120px
     }  
-    [data-testid="stBaseButton-secondary"]:hover{
-        border-color: grey;
-        -webkit-text-fill-color: grey;
-    }
     [data-testid="stBaseButton-headerNoPadding"] {
+        visibility: hidden;
+    }
+    [data-testid="stSidebar"] {
         visibility: hidden;
     }
     </style>
     '''
     # Med hjälp av markdown kan vi använda css
-st.markdown(Background_Page, unsafe_allow_html=True)
-    
-with st.container(border=True,height=250):
-    user_name = st.text_input("Username", max_chars=16)
-    finish = st.button("Done")
+    st.markdown(Background_Page, unsafe_allow_html=True)
+        
+    with st.container(border=True,height=250):
+        user_name = st.text_input(f"{"👤"} Username", max_chars=16)
+        finish = st.button("Done")
+    if user_name and finish:
+        st.switch_page("pages/Home_Page.py")
+    elif not user_name and finish:
+        st.error("Enter your name please")
+
+Welcome_Page()
