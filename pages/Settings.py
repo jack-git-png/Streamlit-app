9b4f50c1133b664a0e4ac55989cfceba7835e2b2
jack-git-png <jack.minayerdji@elev.ga.ntig.se>
2024-10-06 23:44:25 +0200

    Settings.py

--- pages/Settings.py
+++ pages/Settings.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
 
+navigation_bar = option_menu(
+    menu_title=None,
+    icons=("house-door","gear"),
+    options=["Home","Documment"],
+    default_index=1,
+    orientation="horizontal")
